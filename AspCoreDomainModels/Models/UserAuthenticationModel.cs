85f6338e3c23ee27ce0cc31365199c0f2a291f16
Nitin Padade <padade.nitin@gmail.com>
2019-09-04 16:45:17 +0530

    modified api and angular ui code

--- AspCoreDomainModels/Models/UserAuthenticationModel.cs
+++ AspCoreDomainModels/Models/UserAuthenticationModel.cs
@@ -10,7 +10,7 @@ namespace AspCoreDomainModels.Models
 
         public int Id { get; set; }
 
-        public int RoleId { get; set; }
+        public int? RoleId { get; set; }
 
         public string Role { get; set; }
 
